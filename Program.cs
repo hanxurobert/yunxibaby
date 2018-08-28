f64fbba5af8046fbc67c7742dfd4c5476f60f699
Robert Han <hanxurobert@gmail.com>
2018-08-27 21:45:38 -0500

    Add initial files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace YunxiBaby {
+    public class Program {
+        public static void Main (string[] args) {
+            var config = new ConfigurationBuilder ()
+                .SetBasePath (Directory.GetCurrentDirectory ())
+                .AddEnvironmentVariables ()
+                .AddJsonFile ("certificate.json", optional : true, reloadOnChange : true)
+                .AddJsonFile ($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional : true, reloadOnChange : true)
+                .Build ();
+
+            var certificateSettings = config.GetSection ("certificateSettings");
+            string certificateFileName = certificateSettings.GetValue<string> ("filename");
+            string certificatePassword = certificateSettings.GetValue<string> ("password");
+
+            var certificate = new X509Certificate2 (certificateFileName, certificatePassword);
+
+            CreateWebHostBuilder (args)
+                .UseKestrel (options => {
+                    options.AddServerHeader = false;
+                    options.Listen (IPAddress.Loopback, 5001, listenOptions => {
+                        listenOptions.UseHttps (certificate);
+                    });
+                })
+                .UseConfiguration (config)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup>();
+    }
+}
